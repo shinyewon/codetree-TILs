5245b7dc0795cc335f36a05974c4ecdaa03dfca8
yewonshin <rac8793@naver.com>
2023-09-04 23:42:18 +0900

    격자 안에서 여러 객체를 이동

--- Intermediate_Low/01_Simulation/벽이 없는 충돌 실험.py
+++ Intermediate_Low/01_Simulation/벽이 없는 충돌 실험.py
@@ -1,51 +1,50 @@
-# Approach 1: 1초 간격으로 움직임을 직접 시뮬레이션 [Time Limit Exceed]
-
+# Approach 2: 1초 간격으로 움직임을 직접 시뮬레이션 + 충돌을 2차원 배열로 확인
+# 코드 미완성
 T = int(input())  # 테스트 케이스 수
 dtoN = {'U': 0, 'D': 1, 'L': 2, 'R': 3}
-dxs, dys = [0, 0, -0.5, 0.5], [0.5, -0.5, 0, 0]
-max_xy = 1
+dis, djs = [-1, 1, 0, 0], [0, 0, -1, 1]
 for _ in range(T):
-    balls = []
+    max_ij = 0
+    marbles = []
     N = int(input())  # 구슬 개수
-    for i in range(1, N+1):
+    for num in range(1, N+1):
         x, y, w, d = input().split()
-        x, y, w, dNum = float(x), float(y), int(w), dtoN[d]
-        if abs(x) > max_xy:
-            max_xy = abs(x)
-        if abs(y) > max_xy:
-            max_xy = abs(y)
-        balls.append([i, x, y, w, dNum])
+        i, j, w, dNum = (int(x) + 1000) * 2, (1000 -
+                                              int(y)) * 2, int(w), dtoN[d]
+        if i > max_ij:
+            max_ij = i
+        if j > max_ij:
+            max_ij = j
+        marbles.append([num, i, j, w, dNum])
+
+    def in_range(i, j):
+        return i >= 0 and i < max_ij + 1 and j >= 0 and j < max_ij + 1
 
     colT = -1
-    for t in range(1, int(max_xy*4+1)):
+    for t in range(1, max_ij+1):
+        next_marbles = []
+        board = [
+            [[] for _ in range(max_ij+1)]
+            for _ in range(max_ij+1)
+        ]
         # 이동
-        for ball in balls:
-            ball[1], ball[2] = ball[1] + dxs[ball[4]], ball[2] + dys[ball[4]]
-        # 충돌 처리
-        for k in range(len(balls)):
-            if k < len(balls):
-                dups = [balls[k]]
-                pops = []
-                for k2 in range(len(balls[k+1:])):
-                    if balls[k][1] == balls[k+1:][k2][1] and balls[k][2] == balls[k+1:][k2][2]:
-                        dups.append(balls[k+1:][k2])
-                        pops.append(k+1 + k2)
-
-                cnt = 0
-                for i in pops:
-                    balls.pop(i-cnt)
-                    cnt += 1
-
-                if len(dups) > 1:
-                    balls.pop(k)
+        for marble in marbles:
+            marble[1], marble[2] = marble[1] + \
+                dis[marble[4]], marble[2] + djs[marble[4]]
+            if in_range(marble[1], marble[2]):
+                if board[marble[1]][marble[2]] == []:
+                    board[marble[1]][marble[2]] = marble
+                    next_marbles.append(marble)
+                else:
                     colT = t
-                    max_ball = [0, 0, 0, 0, 0]
-                    for dup in dups:
-                        if dup[3] > max_ball[3]:
-                            max_ball = dup
-                        elif dup[3] == max_ball[3]:
-                            if dup[0] > max_ball[0]:
-                                max_ball = dup
-                    balls = [max_ball] + balls
+                    if board[marble[1]][marble[2]][3] < marble[3]:
+                        board[marble[1]][marble[2]] = marble
+                    elif board[marble[1]][marble[2]] == marble[3]:
+                        if board[marble[1]][marble[2]][0] < marble[0]:
+                            board[marble[1]][marble[2]] = marble
+
+        marbles = []
+        for marble in next_marbles:
+            marbles.append(marble)
 
     print(colT)
