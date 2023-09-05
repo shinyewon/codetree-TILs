de396fcc60e125e054020b7d33fed31fe67c4720
yewonshin <rac8793@naver.com>
2023-09-05 23:59:35 +0900

    격자 안에서 여러 객체를 이동

--- Intermediate_Low/01_Simulation/벽이 없는 충돌 실험.py
+++ Intermediate_Low/01_Simulation/벽이 없는 충돌 실험.py
@@ -1,5 +1,5 @@
 # Approach 2: 1초 간격으로 움직임을 직접 시뮬레이션 + 충돌을 2차원 배열로 확인
-# 코드 미완성
+# 메모리 초과
 T = int(input())  # 테스트 케이스 수
 dtoN = {'U': 0, 'D': 1, 'L': 2, 'R': 3}
 dis, djs = [-1, 1, 0, 0], [0, 0, -1, 1]
@@ -9,8 +9,8 @@ for _ in range(T):
     N = int(input())  # 구슬 개수
     for num in range(1, N+1):
         x, y, w, d = input().split()
-        i, j, w, dNum = (int(x) + 1000) * 2, (1000 -
-                                              int(y)) * 2, int(w), dtoN[d]
+        i, j, w, dNum = int(x) * 2 + 2000, (-int(y)) * \
+            2 + 2000, int(w), dtoN[d]
         if i > max_ij:
             max_ij = i
         if j > max_ij:
@@ -22,6 +22,8 @@ for _ in range(T):
 
     colT = -1
     for t in range(1, max_ij+1):
+        print(t)
+        next_marble_index = []
         next_marbles = []
         board = [
             [[] for _ in range(max_ij+1)]
@@ -34,17 +36,18 @@ for _ in range(T):
             if in_range(marble[1], marble[2]):
                 if board[marble[1]][marble[2]] == []:
                     board[marble[1]][marble[2]] = marble
-                    next_marbles.append(marble)
+                    next_marble_index.append([marble[1], marble[2]])
                 else:
                     colT = t
                     if board[marble[1]][marble[2]][3] < marble[3]:
                         board[marble[1]][marble[2]] = marble
-                    elif board[marble[1]][marble[2]] == marble[3]:
-                        if board[marble[1]][marble[2]][0] < marble[0]:
-                            board[marble[1]][marble[2]] = marble
+                    elif board[marble[1]][marble[2]] == marble[3] and board[marble[1]][marble[2]][0] < marble[0]:
+                        board[marble[1]][marble[2]] = marble
 
-        marbles = []
-        for marble in next_marbles:
-            marbles.append(marble)
+        for i in range(len(next_marble_index)):
+            next_marbles.append(
+                board[next_marble_index[i][0]][next_marble_index[i][1]])
+
+        marbles = next_marbles[:]
 
     print(colT)
