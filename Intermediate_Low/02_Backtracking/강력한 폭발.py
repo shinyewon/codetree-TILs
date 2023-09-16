b5f28e449cfe70526e59dfac302b2bfe10d35334
yewonshin <rac8793@naver.com>
2023-09-16 23:46:34 +0900

    K개 중 하나를 N번 선택하기(Simple)

--- Intermediate_Low/02_Backtracking/강력한 폭발.py
+++ Intermediate_Low/02_Backtracking/강력한 폭발.py
@@ -1,3 +1,4 @@
+# 문제 해결 중...
 n = int(input())
 grid = [
     list(map(int, input().split()))
@@ -9,5 +10,10 @@ cnt = 0
 def in_range(i, j):
     return i >= 0 and i < n and j >= 0 and j < n
 
+
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 1:
+            pass
 
 print(cnt)
