2a851815df2ce22dee46a115ed9e95fa192dcb82
yewonshin <rac8793@naver.com>
2023-10-08 22:30:58 +0900

    K개 중 하나를 N번 선택하기(Simple)

--- Intermediate_Low/02_Backtracking/사다리 타기.py
+++ Intermediate_Low/02_Backtracking/사다리 타기.py
@@ -1,23 +1,29 @@
 # 문제 링크: https://www.codetree.ai/missions/2/problems/ladder-game?&utm_source=clipboard&utm_medium=text
-# 소요 시간: 2시간
+# 소요 시간: 3시간
 # 미해결
 
 n, m = map(int, input().split())
-lines = [[] for _ in range(n)]
+lines = [
+    [0 for _ in range(n+1)]
+    for _ in range(m+1)
+]
 for _ in range(m):
     a, b = map(int, input().split())
-    lines[a].append(b)
+    lines[b][a] = 1
+    lines[b][a+1] = 1
 
 # 한 사람의 결과(위치)를 반환
 
 
 def go(person, lines):
     global n
-    cur_b, cur_j = 1, person
-    while (True):
-        pass
-        # 더이상 옆으로 이동할 수 없으면 break
-        # cur_b보다 아래에 있는 가로줄 중 맨 위 가로줄로 이동
+    cur_i, cur_j = 1, person
+    for i in range(1, n+1):
+        if lines[i][cur_j] == 1:
+            if 0 < cur_j - 1 and lines[i][cur_j-1] == 1:
+                cur_j -= 1
+            elif cur_j + 1 <= n and lines[i][cur_j+1] == 1:
+                cur_j += 1
     return cur_j
 
 # 게임의 결과를 반환
@@ -29,12 +35,12 @@ def ghostLeg(lines):
     result = [-1 for _ in range(n)]
     for person in range(1, n+1):
         pos = go(person, lines)
-        result[pos] = person
+        result[pos-1] = person
     return result
 
+
+'''
 # 게임 결과의 일치 여부 반환
-
-
 def checkRes(init_res, lines):
     global n
     global m
@@ -47,28 +53,45 @@ def checkRes(init_res, lines):
             isSame = False
             break
     return isSame
-
+'''
 # 가능한 가로줄 조합들을 모두 찾아준다.
 
 
-def lineCmb(lines, cmb):
+def lineCmb(lines, i, cnt, cmb):
     global cmbs
-    if len(lines) == 0:
+    found = False
+    next_i = 1
+    if cnt == m:
         cmbs.append(cmb)
         return
-    lineCmb(lines[1:], cmb)
-    lineCmb(lines[1:], cmb.append(lines[0]))
+    for b in range(i, m+1):
+        for j in range(1, n+1):
+            if lines[b][j] == 1:
+                found = True
+                next_i = b
+                lines[0][0] += 1
+                lineCmb(lines, next_i, cnt+1, lines)
+                lines[0][0] -= 1
+                lines[b][j] = 0
+                if j-1 > 0 and lines[b][j-1] == 1:
+                    lines[b][j-1] = 0
+                else:
+                    lines[b][j+1] = 0
+                break
+        if found:
+            break
+    lineCmb(lines, next_i, cnt+1, lines)
 
 
 init_res = ghostLeg(lines)
-min_m = m               # 최소 가로줄 수
+min_m = m  # 최소 가로줄 수
 
 cmbs = []
-lineCmb(lines, [])
+lineCmb(lines, 1, 0, [])
+print(cmbs)
 for cmb in cmbs:
-    if len(cmb) < min_m:
-        res = checkRes(init_res, cmb)
-        if res != []:
-            min_m = len(cmb)
+    if cmb[0][0] < min_m:
+        if init_res == ghostLeg(cmb):
+            min_m = cmb[0][0]
 
 print(min_m)
